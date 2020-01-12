3ec57f3a789aba7e631443a5af4660f41ecf2c86
kim <kim@kimui-Macmini.local>
2020-01-13 00:52:45 +0900

    feat: lombok test

--- src/main/java/com/jojoldu/spring/practice/web/HelloController.java
+++ src/main/java/com/jojoldu/spring/practice/web/HelloController.java
@@ -3,6 +3,7 @@ package com.jojoldu.spring.practice.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloController {
     @GetMapping("/hello")
